4f39db90e782aabcb8310627c39f895b05ee9bb6
Haqiim <hqcoder.jh@gmail.com>
2024-08-15 15:48:08 +0100

    #

--- src/components/filternav.jsx
+++ src/components/filternav.jsx
@@ -22,11 +22,19 @@ export default function Filternav() {
   }
 
   return (
-    <div className="categories bg-basecolor flex items-center justify-center top-[54px] w-full fixed pr-padding6 h-[58px]">
-      <div className=' top-[0px] left-[0px] z-[10] absolute h-full w-[100px]'>
-        {display == true ? <div className='curtain p-padding4 flex justify-end'>
+    <div className="categories bg-basecolor flex items-center justify-center top-[54px] w-full fixed h-[58px] 
+    md:w-full md:pr-padding6">
+      <div className=' top-[0px] left-[0px] z-[10] absolute h-full w-[150px]'>
+        {display == true ? <div className='curtain pointer-none absolute top-0  left-[-3px] h-full p-padding4 w-[100px] flex justify-end
+        md:w-[150px]'>
           <img src="/left.svg" alt="" />
         </div> : <></>}
+      </div>
+      <div className=' top-[0px] right-[30px] hidden z-[10] absolute h-full w-[120px] 
+      md:flex'>
+        <div className='curtain2 h-full pl-padding1 p-padding4 w-[120px] absolute justify-start items-center'>
+          <img className='tranform rotate-[180deg]' src="/left.svg" alt="" />
+        </div>
       </div>
       <div onScroll={(e)=>{getScroll(e)}} className='filterbar flex justify-start h-full py-padding1 px-padding6 w-auto pr-[100px] items-center flex gap-gap2 overflow-x-scroll'>
         {
--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -16,30 +16,35 @@ export default function Nav() {
           </div>
           <div>WeTube</div>
       </div>
-      <div className='w-[57%] flex justify-between items-center gap-gap3 h-full'>
+      <div className='flex auto w-justify-between items-center gap-gap3 h-full 
+      md:w-[57%]'>
         <div className='h-[40px] justify-right rounded-rounded5 flex w-auto flex-1'>
-          <div className={`${show? "ml-[0px] border-[#245cd6]" : "ml-[40px] border-glass1"} flex justify-end flex-1 relative items-center w-auto rounded-tl-rounded5 rounded-bl-rounded5 border-[solid] border-[1px] h-full`}>
+          <div className={`${show? "ml-[0px] border-[#245cd6]" : "ml-[40px] border-glass1"} flex justify-end flex-1 relative items-center w-auto rounded-tl-rounded5 rounded-bl-rounded5 h-full 
+            md:border-[solid] md:border-[1px]`}>
             {
               show? <div className='w-[40px] h-full p-[7px] rounded-tl-rounded5 rounded-bl-rounded5'>
                 <img src="/search.svg" alt="" />
               </div>:<></>
             }
-            <input onClick={(e)=>{showIcon(e)}} onBlur={(e)=>{showIcon(e)}} className='w-auto flex-1 ml-[15px] font-norm h-[50%] pb-[1px] border-[0px] bg-[transparent] outline-[0px] caret-textcolor ' type="text" placeholder='Search'/>
+            <input onClick={(e)=>{showIcon(e)}} type="text" placeholder='Search' onBlur={(e)=>{showIcon(e)}} className='w-auto hidden flex-1 ml-[15px] font-norm h-[50%] pb-[1px] border-[0px] bg-[transparent] outline-[0px] caret-textcolor 
+            md:flex'/>
           </div>
-          <button className='h-full w-[65px] p-[7px] rounded-tr-rounded5 border-[solid] border-[1px] border-glass1 rounded-br-rounded5 bg-glass'>
+          <button className='rounded-_rounded p-[7px] h-[40px] w-[40px] border-[solid] border-glass1
+            md:h-full md:w-[65px] md:rounded-bl-[0px] md:rounded-br-rounded5 md:rounded-tl-[0px] md:rounded-tr-rounded5 md:border-[1px] md:bg-glass'>
             <img src="/search.svg"  alt="icon" />
           </button>
         </div>
-        <div className='bg-glass w-[40px] p-[6px] h-[40px] rounded-_rounded '>
+        <div className=' hidden bg-glass w-[40px] p-[6px] h-[40px] rounded-_rounded md:flex '>
           <img src="/mic.svg"  alt="icon" />
         </div>
       </div>
 
-      <div className=' w-auto items-center gap-gap4 flex  justify-between h-full'>
+      <div className=' w-auto items-center gap-gap1 flex  justify-between h-full md:gap-gap4'>
           <div className='w-[32px] h-[32px] p-[2px]'>
             <img src="/vid.svg"  alt="icon" />
           </div>
-          <div className='p-[3px] w-[35px] h-[35px] rounded-_rounded hover:bg-glass'>
+          <div className='p-[3px] w-[35px] h-[35px] hidden rounded-_rounded hover:bg-glass
+          md:flex'>
             <img src="/notes.svg"  alt="icon" />
           </div>
           <div className='bg-[brown] font-bold w-[32px] h-[32px] flex justify-center items-center text-textcolor rounded-_rounded '>A</div>
